25321a3ff4f7332bc53b0ab0cb629e4148e89304
2153749860 <tiagorocha1515@gmail.com>
2019-10-20 15:15:09 +0100

    Calculadora PostFix com ArrayStack completo

--- src/ficha3edp2ex1/Ficha3EDP2Ex1.java
+++ src/ficha3edp2ex1/Ficha3EDP2Ex1.java
@@ -5,6 +5,9 @@
  */
 package ficha3edp2ex1;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author tiago
@@ -16,6 +19,20 @@ public class Ficha3EDP2Ex1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        PostFix calc = new PostFix();
+        
+        try {
+            System.out.println(calc.calcular("1 2 +"));
+            System.out.println(calc.calcular("3 5 + +"));
+            System.out.println(calc.calcular("10 5 -"));
+            System.out.println(calc.calcular("2 2 *"));
+            System.out.println(calc.calcular("4 2 /"));
+            System.out.println(calc.calcular("4 -2 /"));
+        } catch (EmptyCollectionException ex) {
+            Logger.getLogger(Ficha3EDP2Ex1.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
